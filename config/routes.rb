cb1c080ef97ef5f726842785ea11f087431e6b86
Alinawaz-777 <55326140+Alinawaz-786@users.noreply.github.com>
2023-07-17 18:19:25 +0500

    e-commerce_website #1

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+
+
+  devise_for :admins , controllers: {
+        sessions: 'admins/sessions',
+        registrations: 'admins/registrations'
+     }
+
+   get '/' => 'home#index'
+
+   resources :suppliers
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
